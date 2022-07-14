e786e85e6e7a0d6f4a493b9c9cb0d7a9cc8ae9f0
dmytro.serbenyuk <dmytro.serbenyuk@n3xt-solutions.com>
2022-07-14 17:55:50 +0300

    new

--- terraform-manifests/c3-locals.tf
+++ terraform-manifests/c3-locals.tf
@@ -8,6 +8,8 @@ locals {
   nic_name = "${var.business_unit}-${var.environment}-${var.network_interface_name}"
   vm_name = "${var.business_unit}-${var.environment}-${var.virtual_machine_name}"
 
+
+
   # Use-case-2: Common tags to be assigned to all resources
   service_name = "Demo Services"
   owner = "Kalyan Reddy Daida"
@@ -18,6 +20,7 @@ locals {
     #Tag2 = "Terraform-Cloud-Demo1-Workspace-Locked"
     #Tag3 = "Terraform-Cloud-Demo1-VCS-Demo"
     #Tag4 = "Terraform-Cloud-Demo1-Auto-Apply-Test"
+
     #Tag5 = "Notifications Testing"
   }
 }
